67a595aaf06bbe41f15605536d023f636e2e7ae8
Luis <luisgmgomes@gmail.com>
2024-02-06 19:21:42 -0300

    first

---
+++ dist/server.js
@@ -0,0 +1,10 @@
+"use strict";
+const addNumber = (a, b) => {
+    return a * b;
+};
+console.log(addNumber(2, 2));
+console.log(addNumber(4, 4));
+console.log(addNumber(6, 4));
+console.log(addNumber(6, 10));
+console.log(addNumber(8, 10));
+//# sourceMappingURL=server.js.map
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+const addNumber = (a: number, b: number) => {
+  return a * b;
+};
+console.log(addNumber(2, 2));
+console.log(addNumber(4, 4));
+console.log(addNumber(6, 4));
+console.log(addNumber(6, 10));
+console.log(addNumber(8, 10));
